849881d72b5963b223470b9ef5d680d5c08b4563
xin <xin.li.phy@gmail.com>
2021-12-30 11:40:22 -0500

    ch.4 class_add_files

---
+++ Work/stock.py
@@ -0,0 +1,17 @@
+# stock.py
+
+# Stock class
+
+
+class Stock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, qty):
+        self.shares -= qty
+
