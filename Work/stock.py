b9ffd2fcd5ded5d76615bd17cc3b0ab63539aa12
xin <xin.li.phy@gmail.com>
2021-12-30 16:32:59 -0500

    Ch.5 encapsulation

--- Work/stock.py
+++ Work/stock.py
@@ -4,6 +4,7 @@
 
 
 class Stock:
+    __slots__ = ('name', '_shares', 'price')
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
@@ -15,8 +16,19 @@ class Stock:
     def __str__(self):
         return f"({self.name},{self.shares},{self.price})"
 
+    @property
     def cost(self):
         return self.shares * self.price
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Expected int for shares")
+        self._shares = value
 
     def sell(self, qty):
         self.shares -= qty
