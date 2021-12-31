485c228f10228e60755457379f11cceb0bff2371
xin <xin.li.phy@gmail.com>
2021-12-31 18:44:55 -0500

    ch.9 dist

---
+++ porty-app/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Name",
+    author_email="name@example.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
+
